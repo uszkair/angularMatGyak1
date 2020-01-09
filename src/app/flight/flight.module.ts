fe6e3889a54b4ff9edb6e7de6f825f1414c8b9e9
inf_uszkai_r <Robert.Uszkai@groupama.hu>
2020-01-09 14:18:16 +0100

    cuna vagy

---
+++ src/app/flight/flight.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FlightComponent } from './flight.component';
+
+
+
+@NgModule({
+  declarations: [FlightComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class FlightModule { }
